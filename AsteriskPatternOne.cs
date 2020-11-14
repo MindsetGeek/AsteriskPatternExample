3513142a9d46888727e7c1652745188545ec5cb5
dspdamith <damithchandrasekara465@gmail.com>
2020-11-14 07:15:20 +0530

    Add project files.

---
+++ AsteriskPatternOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteriskPatternExample
+{
+    public class AsteriskPatternOne
+    {
+        public void PatternOneShow()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteriskPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsteriskPatternOne obj = new AsteriskPatternOne();
+            obj.PatternOneShow();
+
+
+            Console.ReadLine();
+        }
+    }
+}
