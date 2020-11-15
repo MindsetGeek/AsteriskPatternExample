a67c016d59f186a1f6ac1b0cdb887828313bd95a
dspdamith <damithchandrasekara465@gmail.com>
2020-11-15 11:17:41 +0530

    Foreach loop example

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsteriskPatternExample.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,29 @@ namespace AsteriskPatternExample
     {
         static void Main(string[] args)
         {
-            WhileLoopExample obj = new WhileLoopExample();
-            obj.Examplehile();
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { Id=1, Name="Sanju", Age=22});
+            students.Add(new Student() { Id=2, Name="ABC", Age=23});
+            students.Add(new Student() { Id=3, Name="DEF", Age=25});
+            students.Add(new Student() { Id=4, Name="XXX", Age=25});
+            students.Add(new Student() { Id=5, Name="PPPP", Age=24});
+            students.Add(new Student() { Id=6, Name="Sanju", Age=29});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Id : " + student.Id+ ", Name : " + student.Name+ ", Age : " + student.Age);
+            }
 
             Console.ReadLine();
         }
