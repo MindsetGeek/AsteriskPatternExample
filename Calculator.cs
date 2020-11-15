f33055887eec533cbda804830df9775e1aac67e1
dspdamith <damithchandrasekara465@gmail.com>
2020-11-15 14:01:07 +0530

    While loop example

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteriskPatternExample
+{
+    public class Calculator
+    {
+        public void CalcutorStart()
+        {
+            
+            int p = 0;
+
+            while (p!=2)
+            {
+                Console.Write("Please Enter First Number : ");
+                int firstnum = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Please Enter First Number : ");
+                int secondnum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sum of ( "+ firstnum+" + "+ secondnum + " )"+ (firstnum+ secondnum));
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Please select the option ");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("| 1 - Continue....         |");
+                Console.WriteLine("| 2 - Exit....             |");
+                Console.WriteLine("----------------------------");
+                p = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,32 +10,11 @@ namespace AsteriskPatternExample
     class Program
     {
         static void Main(string[] args)
-        {
-            List<Student> students = new List<Student>();
-            students.Add(new Student() { Id=1, Name="Sanju", Age=22});
-            students.Add(new Student() { Id=2, Name="ABC", Age=23});
-            students.Add(new Student() { Id=3, Name="DEF", Age=25});
-            students.Add(new Student() { Id=4, Name="XXX", Age=25});
-            students.Add(new Student() { Id=5, Name="PPPP", Age=24});
-            students.Add(new Student() { Id=6, Name="Sanju", Age=29});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
-            students.Add(new Student() { Id=7, Name="DDDD", Age=30});
+        {           
+            Calculator obj = new Calculator();
+            obj.CalcutorStart();
 
-            foreach (var student in students)
-            {
-                Console.WriteLine("Id : " + student.Id+ ", Name : " + student.Name+ ", Age : " + student.Age);
-            }
-
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
